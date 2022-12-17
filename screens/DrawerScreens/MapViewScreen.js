2b12b3aa91b87fdec60a89156c8ca7718a961681
armandev19 <107536369+armandev19@users.noreply.github.com>
2022-12-17 11:27:06 +0800

    update 2022-12-17

--- screens/DrawerScreens/MapViewScreen.js
+++ screens/DrawerScreens/MapViewScreen.js
@@ -16,7 +16,13 @@ const MapViewScreen = () => {
   const [userData, setUserCollection] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const test1 = useSelector(selectUserData);
+  const test = [{latitude: 10.629551823160902, longitude: 122.9372239864562, title: 'balay'},
+  {latitude: 10.704628013409447, longitude: 122.9638001368752, title: 'balay'},
+  {latitude: 10.704908697504395, longitude: 122.96321005088977, title: 'balay'},
+  {latitude: 10.705330382048308, longitude: 122.96268970233933, title: 'balay'},
+  {latitude: 10.70387152938821, longitude: 122.96299811217332, title: 'balay'}]
+
+  const currentUserData = useSelector(selectUserData);
 //   const userData = async () => {
 //     try {
 //       const value = await AsyncStorage.getItem('user_id').then(JSON.parse).then(value => {
@@ -37,7 +43,6 @@ const getAllUsers = () => {
   fetch('http://192.168.7.196/bloodmap/fetchUsers.php', {
     method: 'POST',
     headers: {
-      //Header Defination
       'Content-Type':
       'application/x-www-form-urlencoded;charset=UTF-8',
     },
@@ -54,44 +59,62 @@ const getAllUsers = () => {
     });
 }
 
-useFocusEffect(
-  React.useCallback(() => {
+useEffect(()=>{
+  getAllUsers();
+  console.log("userdata", userData);
+}, [])
+
+// useFocusEffect(
+//   React.useCallback(() => {
     
-    getAllUsers();
-    console.log("userdata", test1);
-  }, []),
-);
+//     getAllUsers();
+//     console.log("userdata", userData);
+//   }, []),
+// );
   return (
     <View style={styles.container}>
       <Loader loading={loading} />
       <MapView
       style={styles.map}    
-      //specify our coordinates.
       initialRegion={{
-        latitude: 10.629551823160902,
-        longitude: 122.9372239864562,
+        latitude: currentUserData ? parseFloat(currentUserData.latitude) : 10.653987805083476,
+        longitude: currentUserData ? parseFloat(currentUserData.longitude) : 122.97906514190291,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       }}
       >
-        <Marker
-          coordinate={{ latitude : parseFloat(test1.latitude), longitude : parseFloat(test1.longitude) }}
-          description="This is a marker in React Natve"
-        />
-        {/* {(() => {
-        if (userData) { */}
-          {/* {userData.map((location, i) => {
+      {(() => {
+        if (currentUserData) { 
+          return (
+            <Marker
+              title="This is you."
+              coordinate={{ latitude : parseFloat(currentUserData.latitude), longitude : parseFloat(currentUserData.longitude) }}
+            />
+          )
+        }
+      })()}
+          {/* {test.map((location, i) => {
             return (
               <Marker
               key={i}
               title={location.title}
-              coordinate={{ latitude : location.latitude ? "" :  10.629551823160902, longitude : location.longitude ? "" : 122.9372239864562 }}
+              coordinate={{ latitude : location.latitude, longitude : location.longitude }}
               description="This is a marker in React Natve"
             />
             )
           })} */}
-      {/* }
-    })()} */}
+      
+           {userData.map((location, i) => {
+            return (
+              <Marker
+              key={i}
+              title={location.title}
+              coordinate={{ latitude : parseFloat(location.latitude), longitude : parseFloat(location.longitude) }}
+              description="This is a marker in React Natve"
+            />
+            )
+          })}
+        
         
       
       </MapView>
